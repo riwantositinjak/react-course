4b813d3f32bccfef9985267949752ce36a223245
riwantositinjak <riwanto1212@gmail.com>
2022-11-11 06:19:09 +0700

    react-styling-completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,39 @@
-//Create a react app from scratch.
-//It should display a h1 heading.
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
 import React from "react";
 
-const fname = "Riwanto";
-const lname = "Sitinjak";
-const number = 7;
+const date = new Date();
+const currentTime = date.getHours();
+
+let greeting;
+
+const customStyle = {
+  color: "",
+};
+
+if (currentTime < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (currentTime < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "green";
+} else {
+  greeting = "Good Night";
+  customStyle.color = "blue";
+}
 
 const App = () => {
   return (
-    <div>
-      <h1>
-        Hello {fname} {lname}
-      </h1>
-      <p>my lucky number is {number}</p>
-    </div>
+    <h1 className="heading" style={customStyle}>
+      {greeting}
+    </h1>
   );
 };
+
+//Create a React app from scratch.
+//Show a single h1 that says "Good morning" if between midnight and 12PM.
+//or "Good Afternoon" if between 12PM and 6PM.
+//or "Good evening" if between 6PM and midnight.
+//Apply the "heading" style in the styles.css
+//Dynamically change the color of the h1 using inline css styles.
+//Morning = red, Afternoon = green, Night = blue.
 
 export default App;
