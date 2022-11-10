428797b77c709fc27ee16b269f75b08fe4fc04df
riwantositinjak <riwanto1212@gmail.com>
2022-11-11 06:26:12 +0700

    react component practice completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,19 +3,13 @@
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
 import React from "react";
-
-const fname = "Riwanto";
-const lname = "Sitinjak";
-const number = 7;
+import Heading from "./Heading";
 
 const App = () => {
   return (
     <div>
-      <h1>
-        Hello {fname} {lname}
-      </h1>
-      <p>my lucky number is {number}</p>
-    </div>
+    <Heading />
+  </div>
   );
 };
 
