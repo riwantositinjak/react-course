09a47473c1c7d66c21478b51af1428f5a9374939
riwantositinjak <riwanto1212@gmail.com>
2022-11-12 06:36:05 +0700

    adding react using es6 spread operator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,52 @@
-//Create a react app from scratch.
-//It should display a h1 heading.
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
-import React from "react";
+import React, { useState } from "react";
 
-const fname = "Riwanto";
-const lname = "Sitinjak";
-const number = 7;
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: "",
+  });
 
-const App = () => {
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setContact((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value,
+      };
+    });
+  }
+
   return (
-    <div>
+    <div className="container">
       <h1>
-        Hello {fname} {lname}
+        Hello {contact.fName} {contact.lName}
       </h1>
-      <p>my lucky number is {number}</p>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          onChange={handleChange}
+          name="fName"
+          value={contact.fName}
+          placeholder="First Name"
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          value={contact.lName}
+          placeholder="Last Name"
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          value={contact.email}
+          placeholder="Email"
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
-};
+}
 
 export default App;
